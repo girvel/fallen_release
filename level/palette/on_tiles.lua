c937a829b511035a12003165eada215db4e5eb13
girvel <widauka@ya.ru>
2026-07-23 16:16:47 +0500

    on_tiles palette

--- level/palette/on_tiles.lua
+++ level/palette/on_tiles.lua
@@ -1,7 +1,31 @@
+local factoring = require("engine.tech.factoring")
 local animated = require("engine.tech.animated")
 
 
 local on_tiles = {}
+
+----------------------------------------------------------------------------------------------------
+-- [SECTION] Atlas
+----------------------------------------------------------------------------------------------------
+
+on_tiles.ATLAS_IMAGE = love.graphics.newImage("assets/atlases/on_tiles.png")
+local packer = factoring.packer(on_tiles.ATLAS_IMAGE)
+
+packer.offset = 0
+for index, codename in ipairs {"toilet", "magazine", "blood", "dirt"} do
+  local i, this_sprite = packer:geti(index)
+  on_tiles[i] = function()
+    return {
+      boring_flag = true,
+      codename = codename,
+      sprite = this_sprite,
+    }
+  end
+end
+
+----------------------------------------------------------------------------------------------------
+-- [SECTION] Entities
+----------------------------------------------------------------------------------------------------
 
 local adjust_rate = function(self)
   self.animation.fps = State.level.water_speed
