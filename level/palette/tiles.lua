472763f8fa51518b7b63e25fd3552a23c69df16f
girvel <widauka@ya.ru>
2026-07-24 12:45:23 +0500

    Tiles walk sounds

--- level/palette/tiles.lua
+++ level/palette/tiles.lua
@@ -1,9 +1,14 @@
+local sound = require("engine.tech.sound")
 local factoring = require("engine.tech.factoring")
 local tiles = {}
 
 tiles.ATLAS_IMAGE = love.graphics.newImage("assets/atlases/tiles.png")
 local packer = factoring.packer(tiles.ATLAS_IMAGE)
 packer.offset = 0
+
+local sounds_walkway = sound.multiple("assets/sounds/walk/walkway", .02)
+local sounds_planks = sound.multiple("assets/sounds/walk/planks", .02)
+local sounds_metal = sound.multiple("assets/sounds/walk/metal", .09)
 
 for _, tuple in ipairs {
   {1, "walkway"},
@@ -22,11 +27,21 @@ for _, tuple in ipairs {
     tuple --[=[@as [integer, string]]=]
   )
   local i, this_sprite = packer:geti(index)
+  local walk_sounds
+  if codename == "walkway" or codename == "dirt" then
+    walk_sounds = sounds_walkway
+  elseif codename == "planks" or codename == "planks_straight" then
+    walk_sounds = sounds_planks
+  elseif codename:starts_with("steel") then
+    walk_sounds = sounds_metal
+  end
+  local sounds = {walk = walk_sounds}
   tiles[i] = function()
     return {
       boring_flag = true,
       codename = codename,
       sprite = this_sprite,
+      sounds = sounds,
     }
   end
 end
