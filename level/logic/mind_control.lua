642558b64ffd86b7f2637e8d2606efdf40eff927
girvel <widauka@ya.ru>
2026-08-13 02:33:50 +0500

    Add palette matching to the smoke shader

--- level/logic/mind_control.lua
+++ level/logic/mind_control.lua
@@ -1,4 +1,3 @@
-local shaders = require("level.shaders")
 local dynamic_canvas = require("engine.tech.dynamic_canvas")
 
 
@@ -8,6 +7,7 @@ local mind_control = {}
 --- @class mind_control_strict: entity_strict
 --- @field _canvases [love.Canvas, love.Canvas]
 --- @field _shader love.Shader
+--- @field _t number
 local methods = {}
 mind_control.mt = {__index = methods}
 
@@ -18,30 +18,57 @@ local this_sprite = {
 
   --- @param entity mind_control
   render = function(self, entity, dt)
-    local prev_shader = love.graphics.getShader()
-    local prev_canvas = love.graphics.getCanvas()
-    love.graphics.setCanvas(entity._canvases[1])
-    love.graphics.setShader(entity._shader)
-      love.graphics.clear()
-      love.graphics.draw(entity._canvases[2])
-    love.graphics.setShader(prev_shader)
-    love.graphics.setCanvas(prev_canvas)
+    entity._t = entity._t + dt
+    local FPS = 10
+    if entity._t >= 1/FPS then
+      entity._t = entity._t - 1/FPS
 
-    entity._canvases[1], entity._canvases[2] = entity._canvases[2], entity._canvases[1]
+      local prev_shader = love.graphics.getShader()
+      local prev_canvas = love.graphics.getCanvas()
+      love.graphics.setCanvas(entity._canvases[1])
+      love.graphics.setShader(entity._shader)
+        love.graphics.clear()
+        love.graphics.draw(entity._canvases[2])
+      love.graphics.setShader(prev_shader)
+      love.graphics.setCanvas(prev_canvas)
+
+      entity._canvases[1], entity._canvases[2] = entity._canvases[2], entity._canvases[1]
+    end
     return entity._canvases[2]
   end,
 }
 
 --- @return mind_control
 mind_control.new = function()
+  local palette do
+    palette = {}
+    local palette_image_data = love.image.newImageData("assets/sprites/palette.png")
+    local w, h = palette_image_data:getDimensions()
+    for x = 0, w - 1 do
+      for y = 0, h - 1 do
+        local r, g, b, a = palette_image_data:getPixel(x, y)
+        if a > 0 then
+          table.insert(palette, {r, g, b, a})
+        end
+      end
+    end
+  end
+
   local e = {
     codename = "mind_control",
     sprite = this_sprite,
     layer = "fx_over_shadows",
     position = Vector.zero,
     _canvases = {},
-    _shader = shaders.load_shader("smoke"),
+    _shader = love.graphics.newShader(
+      love.filesystem.read("level/shaders/smoke.frag"):format(#palette),
+      nil  --- @diagnostic disable-line:param-type-mismatch
+    ),
+    _t = 0,
   }
+
+  e._shader:send("palette", unpack(palette))
+
   e._canvases[1] = dynamic_canvas.new(e._canvases, 1)
   e._canvases[2] = dynamic_canvas.new(e._canvases, 2)
 
--- level/shaders/smoke.frag
+++ level/shaders/smoke.frag
@@ -5,16 +5,41 @@ vec2 down = vec2(0., SCALE / love_ScreenSize.y);
 vec2 left = -right;
 vec2 up = -down;
 
+uniform vec4 palette[%s];
+
+vec4 apply_palette(vec4 it)
+{
+    float min_distance = 1;
+    vec4 closest_color;
+    for (int i = 0; i < 46; i++) {
+        vec4 current_color = palette[i];
+        float distance = (
+            pow(current_color.r - it.r, 2) +
+            pow(current_color.g - it.g, 2) +
+            pow(current_color.b - it.b, 2)
+        );
+
+        if (distance < min_distance) {
+            min_distance = distance;
+            closest_color = current_color;
+        }
+    }
+    return closest_color;
+}
+
 vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
 {
-    if (texture_coords.y > 1 - 1 / love_ScreenSize.y) {
+    if (texture_coords.y > 1 - SCALE / love_ScreenSize.y
+        || texture_coords.y < SCALE / love_ScreenSize.y
+        || texture_coords.x > 1 - SCALE / love_ScreenSize.x
+        || texture_coords.x < SCALE / love_ScreenSize.x) {
         return vec4(0, 0, 0, 1);
     }
 
-    return vec4((
+    return apply_palette(vec4((
         + Texel(tex, texture_coords + up)
         + Texel(tex, texture_coords + left)
         + Texel(tex, texture_coords + right)
         + 2 * Texel(tex, texture_coords + down)
-    ).rgb / 5, 1.);
+    ).rgb / 5, 1.));
 }
